12e87ebfc54879a0d6dc7f3c35d1edce67a3c710
Bikiscripter <Sumikalita99999999@gmail.com>
2021-12-02 07:56:41 +0530

    fast

---
+++ main.js
@@ -0,0 +1,98 @@
+
+const updateLSData = () => {
+  
+  const textAreaData = document.querySelectorAll("textarea");
+  const notes = [];
+  //console.log(textAreaData);
+  textAreaData.forEach((note) => {
+    
+    return notes.push(note.value);
+  });
+ // console.log(notes);
+
+  localStorage.setItem("notes", JSON.stringify(notes));
+};
+
+// <!-- SUbSCRIBE THAPA TECHNICAL YOUTUBE CHANNEL https://www.youtube.com/thapatechnical -->
+
+const addNewNote = (text = "") => {
+  
+  const note = document.createElement("div");
+  note.classList.add("note");
+
+  const htmlData = `
+    <div class="operation">
+        <button class="edit"> <i class="fas fa-edit"></i> </button>
+        <button class="delete"> <i class="fas fa-trash-alt"></i> </button>
+    </div>
+
+    <div class="main ${text ? "" : "hidden"} "> </div>
+    <textarea class="${text ? "hidden" : ""}"></textarea>  `;
+
+  note.insertAdjacentHTML("afterbegin", htmlData);
+  // console.log(note);
+
+  // getting the References
+  const editButton = note.querySelector(".edit");
+  const delButton = note.querySelector(".delete");
+  const mainDiv = note.querySelector(".main");
+  const textArea = note.querySelector("textarea");
+
+  // deleting the node
+  delButton.addEventListener("click", () => {
+   //console.log('delete note')
+    note.remove();
+    updateLSData();
+    
+   let ggg =  document.createElement('div')
+   ggg.classList.add("alert")
+   ggg.classList.add("alert-success")
+   ggg.innerHTML = 'Success A Delete Note'
+   let con = confirm("You Serious Delete This Note!");
+   
+  let bhh = document.getElementById('h').appendChild(ggg)
+   setTimeout(function () { 
+
+  
+
+            // Adding the class dynamically 
+
+            bhh.classList.add('hide'); 
+
+        }, 2000); 
+  });
+
+  // toggle using edit button
+  textArea.value = text;
+  mainDiv.innerHTML = text;
+
+  editButton.addEventListener("click", () => {
+    mainDiv.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
+  });
+
+  textArea.addEventListener("change", (event) => {
+    
+    const value = event.target.value;
+    mainDiv.innerHTML = value;
+
+    updateLSData();
+  });
+
+  document.body.appendChild(note);
+  // it appeds a node as the last child of  a node
+  
+};
+
+// getting data back from localStorage
+const notes = JSON.parse(localStorage.getItem("notes"));
+
+if (notes) {
+  notes.forEach((note) => addNewNote(note));
+
+  
+}
+
+//addButton.addEventListener("click", () => addNewNote());
+
+const addButton = document.getElementById("add").addEventListener("click", () => addNewNote());
